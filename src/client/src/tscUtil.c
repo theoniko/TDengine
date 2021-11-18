40dbd2decf6a43957edeb4bfeb0f74939a5e5f43
shenglian zhou <slzhou@taosdata.com>
2021-11-18 21:34:29 +0800

    try to make csum/diff/derivative not project query on stable

--- src/client/src/tscUtil.c
+++ src/client/src/tscUtil.c
@@ -268,11 +268,8 @@ bool tscIsProjectionQueryOnSTable(SQueryInfo* pQueryInfo, int32_t tableIndex) {
         functionId != TSDB_FUNC_TS &&
         functionId != TSDB_FUNC_ARITHM &&
         functionId != TSDB_FUNC_TS_COMP &&
-        functionId != TSDB_FUNC_DIFF &&
-        functionId != TSDB_FUNC_DERIVATIVE &&
-        functionId != TSDB_FUNC_MAVG &&
-        functionId != TSDB_FUNC_CSUM &&
         functionId != TSDB_FUNC_TS_DUMMY &&
+        functionId != TSDB_FUNC_TAG_DUMMY &&
         functionId != TSDB_FUNC_TID_TAG &&
         functionId != TSDB_FUNC_CEIL &&
         functionId != TSDB_FUNC_FLOOR &&
